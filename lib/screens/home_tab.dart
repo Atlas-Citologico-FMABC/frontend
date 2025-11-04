3788538266a74842bd49ba046af0a1004086677b
Luisa2709 <luisalerioleite@gmail.com>
2025-11-03 21:21:15 -0300

    Allow diretórios mais usados text to use darkBlue by removing const keyword

--- lib/screens/home_tab.dart
+++ lib/screens/home_tab.dart
@@ -24,10 +24,7 @@ class HomeTab extends StatelessWidget {
                   padding: const EdgeInsets.all(20),
                   child: Column(
                     children: <Widget>[
-                      Image.asset(
-                        'assets/images/fmabc.png',
-                        height: 80,
-                      ),
+                      Image.asset('assets/images/fmabc.png', height: 80),
                       const SizedBox(height: 20),
                       const Text(
                         'Atlas de Citologia',
@@ -65,11 +62,11 @@ class HomeTab extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        const Align(
+                        Align(
                           alignment: Alignment.centerLeft,
                           child: Text(
                             'Diretórios mais usados:',
-                            style: TextStyle(fontSize: 20),
+                            style: TextStyle(fontSize: 20, color: darkBlue),
                           ),
                         ),
                         const SizedBox(height: 20),
@@ -108,85 +105,88 @@ class HomeTab extends StatelessWidget {
             ),
           );
         }
-    
-      return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            // height: MediaQuery.of(context).size.height - navHeight,
-            // color: darkBlue,
-            padding: EdgeInsets.all(30),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  'Atlas de Citologia',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 45,
-                  ),
-                ),
-                Divider(color: Colors.white),
-                SizedBox(height: 100),
-                Text(
-                  '<descrição do sistema>',
-                  style: TextStyle(color: Colors.white, fontSize: 30),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            constraints: BoxConstraints(
-              minHeight:
-                  MediaQuery.of(context).size.height -
-                  383, // -383 just for tests
-            ),
-            decoration: BoxDecoration(
-              color: lightGray,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.elliptical(700, 70),
-                topRight: Radius.elliptical(700, 70),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 70, horizontal: 80),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Diretórios mais usados:',
-                      style: TextStyle(fontSize: 30),
+
+        return SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                // height: MediaQuery.of(context).size.height - navHeight,
+                // color: darkBlue,
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Atlas de Citologia',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 45,
+                      ),
                     ),
+                    Divider(color: Colors.white),
+                    SizedBox(height: 100),
+                    Text(
+                      '<descrição do sistema>',
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                constraints: BoxConstraints(
+                  minHeight:
+                      MediaQuery.of(context).size.height -
+                      383, // -383 just for tests
+                ),
+                decoration: BoxDecoration(
+                  color: lightGray,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.elliptical(700, 70),
+                    topRight: Radius.elliptical(700, 70),
                   ),
-                  SizedBox(height: 20),
-                  Wrap(
-                    spacing: 50,
-                    runSpacing: 30,
-                    children: <Widget>[
-                      DiretorioBox(
-                        title: 'Diretório 1',
-                        borderWidth: 5,
-                        borderColor: green,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 70,
+                    horizontal: 80,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Diretórios mais usados:',
+                          style: TextStyle(fontSize: 30),
+                        ),
                       ),
-                      DiretorioBox(
-                        title: 'Diretório 2',
-                        borderWidth: 5,
-                        borderColor: green,
-                      ),
-                      DiretorioBox(
-                        title: 'Diretório 3',
-                        borderWidth: 5,
-                        borderColor: green,
-                      ),
-                      DiretorioBox(
-                        title: 'Diretório 4',
-                        borderWidth: 5,
-                        borderColor: green,
+                      SizedBox(height: 20),
+                      Wrap(
+                        spacing: 50,
+                        runSpacing: 30,
+                        children: <Widget>[
+                          DiretorioBox(
+                            title: 'Diretório 1',
+                            borderWidth: 5,
+                            borderColor: green,
+                          ),
+                          DiretorioBox(
+                            title: 'Diretório 2',
+                            borderWidth: 5,
+                            borderColor: green,
+                          ),
+                          DiretorioBox(
+                            title: 'Diretório 3',
+                            borderWidth: 5,
+                            borderColor: green,
+                          ),
+                          DiretorioBox(
+                            title: 'Diretório 4',
+                            borderWidth: 5,
+                            borderColor: green,
                           ),
                         ],
                       ),
@@ -197,7 +197,7 @@ class HomeTab extends StatelessWidget {
             ],
           ),
         );
-      }, 
+      },
     );
-  } 
-} 
+  }
+}
