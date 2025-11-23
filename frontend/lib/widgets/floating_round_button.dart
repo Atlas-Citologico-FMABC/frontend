c2fd03d18a383f05e9a9995b27bf9a535389c609
Vitor Eiken <117394582+eikqn@users.noreply.github.com>
2025-11-23 11:19:16 -0300

    FloatingRoundButton more flexible

--- frontend/lib/widgets/floating_round_button.dart
+++ frontend/lib/widgets/floating_round_button.dart
@@ -2,24 +2,44 @@ import 'package:flutter/material.dart';
 
 class FloatingRoundButton extends StatelessWidget {
   final VoidCallback onPressed;
+	final double? top;
+	final double right;
+	final double bottom;
+	final double? left;
+	final double? width;
+	final double? height;
+	final Color backgroundColor;
   final IconData icon;
+	final Color iconColor;
 
   const FloatingRoundButton({
     super.key,
     required this.onPressed,
-    this.icon = Icons.add, // Ícone padrão
+		this.top,
+		this.right = 20,
+		this.bottom = 20,
+		this.left,
+		this.width,
+		this.height,
+		this.backgroundColor = Colors.green,
+    this.icon = Icons.add,
+		this.iconColor = Colors.white,
   });
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      bottom: 20,
-      right: 20,
+			top: top,
+      right: right,
+      bottom: bottom,
+			left: left,
+			width: width,
+			height: height,
       child: FloatingActionButton(
         onPressed: onPressed,
-        backgroundColor: Colors.green, // verde
+        backgroundColor: backgroundColor,
         shape: const CircleBorder(), // formato redondo
-        child: Icon(icon, color: Colors.white),
+        child: Icon(icon, color: iconColor),
       ),
     );
   }
